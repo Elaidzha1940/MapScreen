feb21f344d0f7c5666b87b22a39ef860dadcf47f
Elaidzha1940 <eli080293@gmail.com>
2023-08-08 16:36:08 +0300

    MapScreen

---
+++ MapScreen/ViewController.swift
@@ -0,0 +1,21 @@
+//  /*
+//
+//  Project: MapScreen
+//  File: ViewController.swift
+//  Created by: Elaidzha Shchukin
+//  Date: 08.08.2023
+//
+//  */
+
+import UIKit
+import MapKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var mapView: MKMapView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
+
